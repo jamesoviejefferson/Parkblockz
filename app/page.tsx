3daaa11bb059ea720866cc848b8bb0d74fd9cde1
James Jefferson <jamesoviejefferson96@gmail.com>
2026-01-05 13:28:21 +0100

    changes reverted

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import WhyNow from "@/components/WhyNow";
-import ContactCTA from "@/components/ContactCTA";
 
 export default function Page() {
   return (
-    <>
-      <Navbar />
+    <> 
       <Hero />
-      <WhyNow />
-      <ContactCTA />
     </>
   );
 }
